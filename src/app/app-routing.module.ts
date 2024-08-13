4899657995b1eb29f252f6dd7747051f6d84e0a8
7o22am <7o22am@gmail.com>
2024-08-13 23:41:38 +0300

    v2.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ChartComponent } from './chart/chart.component';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'chart', component: ChartComponent },
+  { path: 'data', component: SearchFilterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
